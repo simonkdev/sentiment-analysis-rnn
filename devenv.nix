dea77f7832fc36ed55c6493e65c316200d57bfc2
simonkdev <simonkorten09@gmail.com>
2026-05-14 20:09:36 +0200

    added devenv for deps

---
+++ devenv.nix
@@ -0,0 +1,23 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  packages = with pkgs; [
+    git
+    python313Packages.numpy
+    python313Packages.pandas
+    python313Packages.flask
+    python313Packages.cors
+  ];
+
+  languages.python.enable = true;
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+}
