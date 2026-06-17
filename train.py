1ad7b136d82aa7de883c68321acf29e31d3efa4d
simonkdev <simonkorten09@gmail.com>
2026-06-17 08:45:41 +0200

    retrained

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from main_rnn import RNN
 
 rnn = RNN(load_dataset=True)
-rnn.train(iterations=20, batch_size=256)
+rnn.train(iterations=20000, batch_size=256)
 rnn.backprop.save_state()
 rnn.calculate_accuracy()
